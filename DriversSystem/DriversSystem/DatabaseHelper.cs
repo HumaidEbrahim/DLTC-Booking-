596bef275c03723fddb53ddfe8aa477ba5920e65
HumaidEbrahim <32646111+HumaidEbrahim@users.noreply.github.com>
2024-08-21 14:31:18 +0200

    Database Helper
    added execute query method for sql select

---
+++ DriversSystem/DriversSystem/DatabaseHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace DriversSystem
+{
+    public class DatabaseHelper
+    {
+        private static string connString;
+
+         static DatabaseHelper()
+        {
+            connString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        }
+
+        public DataTable ExecuteQuery(string query, SqlParameter[] param = null)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    if (param != null)
+                    {
+                        cmd.Parameters.AddRange(param);
+                    }
+
+                    conn.Open();
+
+                    using (SqlDataAdapter adap = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        adap.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+
+    }
+}
+        
