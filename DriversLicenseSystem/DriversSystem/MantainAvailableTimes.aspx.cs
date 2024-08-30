6a5f1a2198ccacac807df4c3091b1037ef872a7d
46035753 <46035753@mynwu.ac.za>
2024-08-30 11:42:25 +0200

    available times
    available times
    
    Co-Authored-By: Muhammed Erdem <muhammed@erdem.dev>
    Co-Authored-By: Ben-Dover123 <177201038+Ben-Dover123@users.noreply.github.com>

--- DriversLicenseSystem/DriversSystem/MantainAvailableTimes.aspx.cs
+++ DriversLicenseSystem/DriversSystem/MantainAvailableTimes.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -10,30 +11,121 @@ namespace DriversSystem
 {
     public partial class MantainAvailableTimes : System.Web.UI.Page
     {
+
+        DatabaseHelper dbHelper = new DatabaseHelper();
+        DataTable dt = new DataTable();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+                populateGridView();
 
         }
 
         protected void SearchTextBox_TextChanged(object sender, EventArgs e)
         {
-      
+            string userSearch = SearchTextBox.Text.Trim();
+
+            string query = "SELECT * FROM AVAILABLETIME WHERE AvailableTime_Date LIKE '%' + @userSearch + '%'";
+            SqlParameter[] param =
+            {
+                new SqlParameter("@userSearch", SqlDbType.VarChar, 50) { Value = userSearch }
+            };
+
+            AvailableTimesGridView.DataSource = dbHelper.ExecuteQuery(query, param);
+            AvailableTimesGridView.DataBind();
         }
 
 
 
         protected void ConfirmDeleteButton_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                int id = int.Parse(HiddenDelAvailableTimeID.Value);
+
+                string query = "DELETE FROM AvailalbleTime WHERE AvailableTime_ID = @ID";
+                SqlParameter[] param =
+                {
+                    new SqlParameter("@ID", SqlDbType.Int) { Value = id}
+                };
+
+                int result = dbHelper.ExecuteNonQuery(query, param);
+
+                // repopulate gridview and success message
+                if (result > 0)
+                {
+                    successAlert.Visible = true;
+                    successAlert.Controls.Add(new Literal { Text = "Service deleted successfully!" });
+                    populateGridView();
+                }
+            }
+            catch (Exception)
+            {
+                successAlert.Visible = false;
+                errorAlert.Visible = true;
+                errorAlert.Controls.Add(new Literal { Text = "Failed to delete service" });
+            }
         }
 
         protected void ConfirmUpdateButton_Click(Object sender, EventArgs e)
-        {
 
+        {
+            string inputDate = UpdateAvailableDate.Text;
+            double inputTime;
+
+            try
+            {
+                int id = int.Parse(HiddenUpdateAvailableTimeID.Value);
+                inputTime = double.Parse(UpdateAvailableTime.Text);
+
+                string query = "UPDATE Service SET Service_Descr = @Descr, Price = @Price WHERE Service_ID = @ID";
+
+                SqlParameter[] param =
+                {
+                    new SqlParameter("@Descr", SqlDbType.VarChar, 50) { Value = inputDate},
+                    new SqlParameter("@Price", SqlDbType.SmallMoney) { Value =  inputTime },
+                    new SqlParameter("@ID", SqlDbType.Int) { Value = id}
+                 };
+
+                int result = dbHelper.ExecuteNonQuery(query, param);
+
+                if (result > 0)
+                {
+                    // repopulate gridview and success message
+                    populateGridView();
+                    errorAlert.Visible = false;
+                    successAlert.Visible = true;
+                    successAlert.Controls.Add(new Literal { Text = "Service updated successfully!" });
+                }
+            }
+            catch (Exception)
+            {
+                successAlert.Visible = false;
+                errorAlert.Visible = true;
+                errorAlert.Controls.Add(new Literal { Text = "Failed to update service" });
+            }
         }
+
+
+
 
         protected void populateGridView()
         {
+
+            string query = "SELECT * FROM Service";
+            try
+            {
+                AvailableTimesGridView.DataSource = dbHelper.ExecuteQuery(query);
+                AvailableTimesGridView.DataBind();
+            }
+            catch (SqlException)
+            {
+                errorAlert.Visible = true;
+                errorAlert.Controls.Add(new Literal { Text = "Failed to connect to database" });
+            }
+
+
+
 
         }
     }
